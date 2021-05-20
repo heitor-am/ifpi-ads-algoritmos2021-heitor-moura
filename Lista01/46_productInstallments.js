1df156fc7db89eeab322fe2014cefa40d76caecb
Heitor Moura <heitorandrade2001@gmail.com>
2021-05-20 15:47:58 -0300

    ✅ question 46 review

--- Lista01/46_productInstallments.js
+++ Lista01/46_productInstallments.js
@@ -1,13 +1,14 @@
+// Include the prompt-sync module
 const input = require('prompt-sync')();
 
 // Input
 let productValue = Number(input('Informe o valor do produto: '));
 
 // Process
-let installments = Math.trunc((productValue / 3)),
-  inputPayment = Math.trunc((productValue % 3) + p);
+let installment = Math.trunc((productValue / 3)),
+  paymentOfInput = Math.trunc((productValue % 3) + installment);
 
 // Output
-console.log(`-----\nValor da entrada: R$${i.toFixed(2)}`);
-console.log(`Valor da 1° prestação: R$${p.toFixed(2)}`);
-console.log(`Valor da 2° prestação: R$${p.toFixed(2)}`);
+console.log(`-----\nValor da entrada: R$${paymentOfInput.toFixed(2)}`);
+console.log(`Valor da 1° prestação: R$${installment.toFixed(2)}`);
+console.log(`Valor da 2° prestação: R$${installment.toFixed(2)}`);
