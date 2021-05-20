979bef940aaefff69ac29482bdc22c980f31dcec
Heitor Moura <heitorandrade2001@gmail.com>
2021-05-20 00:00:14 -0300

    ✅ question 44 solved

---
+++ Lista01/44_numberOfComponents.js
@@ -0,0 +1,12 @@
+const input = require('prompt-sync')();
+
+// Input
+let quantify = Number(input('Informe a quantidade de latão (em kg): '));
+
+// Process
+let quantifyOfCopper = quantify * 0.7,
+  quantifyOfZinc = quantify * 0.3;
+
+// Output
+console.log(`-----\nQuantidade de cobre: ${quantifyOfCopper.toFixed(1)}kg\n`);
+console.log(`Quantidade de zinco: ${quantifyOfZinc.toFixed(1)}kg`);
