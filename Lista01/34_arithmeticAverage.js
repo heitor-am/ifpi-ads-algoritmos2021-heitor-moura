fbd472fc74a02a1e00f3f606abc31efb1ab613a5
Heitor Moura <heitorandrade2001@gmail.com>
2021-05-21 17:34:45 -0300

    ✅ question 34 review

--- Lista01/34_arithmeticAverage.js
+++ Lista01/34_arithmeticAverage.js
@@ -1,12 +1,13 @@
+// Include the prompt-sync module
 const input = require('prompt-sync')()
 
 // Input
-let number1 = Number(input('Digite o primeiro número: ')),   
+let number1 = Number(input('Digite o primeiro número: ')),
     number2 = Number(input('Digite o segundo número: ')),
     number3 = Number(input('Digite o terceiro número: '));
 
 // Process
-let average = (number1 + number2 + number3)/3;
+let average = (number1 + number2 + number3) / 3;
 
 // Output
 console.log(`-----\nMédia aritmética: ${average.toFixed(2)}`);
