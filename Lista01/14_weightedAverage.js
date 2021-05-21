36ce0a9e4fac72c34d21e632fc1eae2c0a760a5e
Heitor Moura <heitorandrade2001@gmail.com>
2021-05-21 16:40:44 -0300

    ✅ question 14 review

--- Lista01/14_weightedAverage.js
+++ Lista01/14_weightedAverage.js
@@ -1,12 +1,13 @@
+// Include the prompt-sync module
 const input = require('prompt-sync')();
 
 // Input
 console.log('OBS: Separe os valores por vírgula!');
-let [n1, n2, n3] = input('Digite as notas: ').split(','),
-    [p1, p2, p3] = input('Digite os pesos: ').split(',');
+let [n1, n2, n3] = input('Digite as notas: ').split(',').map(value => Number(value)),
+    [p1, p2, p3] = input('Digite os pesos: ').split(',').map(value => Number(value));
 
 // Process
-let weightedAverage = (n1*p1 + n2*p2 + n3*p3)/(Number(p1) + Number(p2) + Number(p3));
+let weightedAverage = ((n1 * p1) + (n2 * p2) + (n3 * p3)) / (p1 + p2 + p3);
 
 // Output
 console.log(`-----\nMédia ponderada: ${weightedAverage.toFixed(2)}`);
