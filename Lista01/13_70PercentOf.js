607dbdbf223a9909a59efb552dde5394f90b11ea
Heitor Moura <heitorandrade2001@gmail.com>
2021-05-21 16:40:29 -0300

    ✅ question 13 review

--- Lista01/13_70PercentOf.js
+++ Lista01/13_70PercentOf.js
@@ -1,7 +1,8 @@
+// Include the prompt-sync module
 const input = require('prompt-sync')();
 
 // Input
 let valueBRL = Number(input('Digite o valor em real (R$): '));
 
 // Output
-console.log(`-----\n70% do valor: ${(valueBRL*0.7).toFixed(2)}`);
+console.log(`-----\n70% do valor: ${(valueBRL * 0.7).toFixed(2)}`);
