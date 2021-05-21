4652ebd962528027253e7d4e8f892bb1dd5f16bd
Heitor Moura <heitorandrade2001@gmail.com>
2021-05-21 17:37:04 -0300

    ✅ question 43 review

--- Lista01/43_equationResolution3.js
+++ Lista01/43_equationResolution3.js
@@ -1,11 +1,12 @@
+// Include the prompt-sync module
 const input = require('prompt-sync')();
 
 // Input
-let [a, b, c, d, e, f] = input('Informe os valores dos coeficientes (A, B, C, D, E, F): ').split(',');
+let [a, b, c, d, e, f] = input('Informe os valores dos coeficientes (A, B, C, D, E, F): ').split(',').map(value => Number(value));
 
 // Process
-let x = ((+c)*(+e) - (+b)*(+f)) / ((+a)*(+e) - (+b)*(+d)),
-  y = ((+a)*(+f) - (+c)*(+d)) / ((+a)*(+e) - (+b)*(+d));
+let x = (c * e - b * f) / (a * e - b * d),
+  y = (a * f - c * d) / (a * e - b * d);
 
 // Output
 console.log(`-----\nValor de x: ${x.toFixed(2)}\nValor de y: ${y.toFixed(2)}`);
