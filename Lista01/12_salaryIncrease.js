0414d96a82b5b977516ba265312646ec6262476c
Heitor Moura <heitorandrade2001@gmail.com>
2021-05-21 16:40:11 -0300

    ✅ question 12 review

--- Lista01/12_salaryIncrease.js
+++ Lista01/12_salaryIncrease.js
@@ -1,10 +1,11 @@
+// Include the prompt-sync module
 const input = require('prompt-sync')();
 
 // Input
 let salary = Number(input('Digite o valor do salário: '));
 
 // Process
-salary += salary*0.25;
+salary += salary * 0.25;
 
 // Output
 console.log(`-----\nSalário com aumento de 25%: R$${salary.toFixed(2)}`);
