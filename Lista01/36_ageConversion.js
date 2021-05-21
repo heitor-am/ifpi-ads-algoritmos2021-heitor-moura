d334cbcf32a8cd13cee01b0ed4ce7f4358999624
Heitor Moura <heitorandrade2001@gmail.com>
2021-05-21 17:35:12 -0300

    ✅ question 36 review

--- Lista01/36_ageConversion.js
+++ Lista01/36_ageConversion.js
@@ -1,10 +1,11 @@
+// Include the prompt-sync module
 const input = require('prompt-sync')();
 
 // Input
 let [years, months, days] = input('Informe a idade da pessoa (anos, meses, dias): ').split(',');
 
 // Process
-days = Number(years)*360 + Number(months)*30 + Number(days);
+days = Number(years) * 365 + Number(months) * 30 + Number(days);
 
 // Output
 console.log(`-----\nIsso corresponde a ${days} dia(s)!`);
