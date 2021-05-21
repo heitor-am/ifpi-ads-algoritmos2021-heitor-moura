14171534693d95b59f201f1223136fe23c3de141
Heitor Moura <heitorandrade2001@gmail.com>
2021-05-21 17:35:50 -0300

    ✅ question 38 review

--- Lista01/38_sumOfFractions.js
+++ Lista01/38_sumOfFractions.js
@@ -1,12 +1,13 @@
+// Include the prompt-sync module
 const input = require('prompt-sync')();
 
 // Input
-let [a, b] = input('Informe a primeira fração (A/B): ').split('/'),
-  [c, d] = input('Informe a segunda fração (C/D): ').split('/');
+let [a, b] = input('Informe a primeira fração (A/B): ').split('/').map(value => Number(value)),
+  [c, d] = input('Informe a segunda fração (C/D): ').split('/').map(value => Number(value));
 
 // Process
-let numerator = (+a)*(+d) + (+c)*(+b),
-  denominator = (+b) * (+d);
+let numerator = (a * d) + (c * b),
+  denominator = b * d;
 
 // Output
 console.log(`-----\nResultado: ${numerator}/${denominator}`);
