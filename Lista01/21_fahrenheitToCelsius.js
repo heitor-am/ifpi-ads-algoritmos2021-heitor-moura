642ee0976307628cc2297705370b98acf4916de0
Heitor Moura <heitorandrade2001@gmail.com>
2021-05-21 16:42:35 -0300

    ✅ question 21 review

--- Lista01/21_fahrenheitToCelsius.js
+++ Lista01/21_fahrenheitToCelsius.js
@@ -1,10 +1,11 @@
+// Include the prompt-sync module
 const input = require('prompt-sync')();
 
 // Input
 let temperature = Number(input('Informe a temperatura em Fahrenheit: '));
 
 // Process
-temperature = (5 * temperature - 160)/9;
+temperature = (5 * temperature - 160) / 9;
 
 // Output
 console.log(`-----\nTemperatura em Celsius: ${temperature.toFixed(2)}`);
