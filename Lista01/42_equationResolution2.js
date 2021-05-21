13d8f1d43c2aa3dcc22c555e52a7bd95215a538f
Heitor Moura <heitorandrade2001@gmail.com>
2021-05-21 17:36:52 -0300

    ✅ question 42 review

--- Lista01/42_equationResolution2.js
+++ Lista01/42_equationResolution2.js
@@ -1,11 +1,12 @@
+// Include the prompt-sync module
 const input = require('prompt-sync')();
 
 // Input
-let [x1, y1] = input('Informe as coordenadas do primeiro ponto (x1, y1): ').split(','),
-  [x2, y2] = input('Informe as coordenadas do segundo ponto (x2, y2): ').split(',');
+let [x1, y1] = input('Informe as coordenadas do primeiro ponto (x1, y1): ').split(',').map(value => Number(value)),
+  [x2, y2] = input('Informe as coordenadas do segundo ponto (x2, y2): ').split(',').map(value => Number(value));
 
 // Process
-let d = Math.sqrt(((+x2) - (+x1))**2 + ((+y2) - (+y1))**2);
+let d = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
 
 // Output
 console.log(`-----\nResultado: ${d.toFixed(2)}`);
