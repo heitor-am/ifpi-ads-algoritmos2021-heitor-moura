079852b69604a8511523da25809b7e3c7be91889
Heitor Moura <heitorandrade2001@gmail.com>
2021-05-21 16:41:28 -0300

    ✅ question 17 review

--- Lista01/17_areaOfRectangle.js
+++ Lista01/17_areaOfRectangle.js
@@ -1,8 +1,9 @@
+// Include the prompt-sync module
 const input = require('prompt-sync')();
 
 // Input
 let base = Number(input('Base do retângulo: ')),
-    height = Number(input('Altura do retângulo: '));
+  height = Number(input('Altura do retângulo: '));
 
 // Process
 let area = base * height;
