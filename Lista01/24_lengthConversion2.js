a2a0da23e5eb55e8ea856715bfb1611053db9731
Heitor Moura <heitorandrade2001@gmail.com>
2021-05-21 16:43:20 -0300

    ✅ question 24 review

--- Lista01/24_lengthConversion2.js
+++ Lista01/24_lengthConversion2.js
@@ -1,7 +1,8 @@
+// Include the prompt-sync module
 const input = require('prompt-sync')();
 
 // Input
 let length = Number(input('Informe o valor em metros: '));
 
 // Output
-console.log(`-----\nEquivalente em centímetros: ${length*100}`);
+console.log(`-----\nEquivalente em centímetros: ${length * 100}`);
