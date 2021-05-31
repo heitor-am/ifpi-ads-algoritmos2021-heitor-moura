2255bef5e16846b0dfffe0dd5a64151aac7c11f2
Heitor Moura <heitorandrade2001@gmail.com>
2021-05-31 18:44:19 -0300

    ✅ question 4 review

--- Lista02/04_significant-digits.js
+++ Lista02/04_significant-digits.js
@@ -2,26 +2,29 @@
 const input = require('prompt-sync')();
 
 function main() {
-  console.log('### Igual ou diferente ###');
+  console.log('### Comparação entre algarismos ###\n');
 
   // Read the user's input
   let n = Number(input('Informe um número (2 dígitos): '));
 
   // Get the result
-  let result = compareSignificantDigits(n);
+  let result = equalSignificantDigits(n);
 
-  // Output
-  console.log(result);
+  if (result) {
+    // Output: if the significant figures are equal
+    console.log('\nAlgarismos iguais!');
+  } else {
+    // Output: if the significant figures aren't equal
+    console.log('\nAlgarismos diferentes!');
+  }
 }
 
 main();
 
 // Check the equality between significant figures
-function compareSignificantDigits(number) {
+function equalSignificantDigits(number) {
   let ten = Math.trunc(number / 10),
     unit = number % 10;
 
-  if (ten === unit) return 'Algarismos iguais!';
-
-  return 'Algarismos diferentes!';
+  return (ten === unit) ? true : false;
 }
