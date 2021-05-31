1d3dcce6c14ba7cec9e0a4f62ec6afef7dc59ec0
Heitor Moura <heitorandrade2001@gmail.com>
2021-05-31 18:42:22 -0300

    ✅ question 1 review

--- Lista02/01_equal-numbers.js
+++ Lista02/01_equal-numbers.js
@@ -2,29 +2,24 @@
 const input = require('prompt-sync')();
 
 function main() {
-  console.log('### Números iguais ###');
+  console.log('### Números iguais ###\n');
 
   // Read the user's input
   let n1 = Number(input('Informe o 1° número: ')),
     n2 = Number(input('Informe o 2° número: ')),
     n3 = Number(input('Informe o 3° número: '));
 
-  // Get the result
-  let result = areEqual(n1, n2, n3);
-
-  // Output
-  console.log(result);
+  // Check how many equal numbers exist between three numbers
+  if (n1 === n2 && n1 === n3) {
+    // Output: if are three numbers equal
+    console.log('\nOs três números são iguais!');
+  } else if (n1 === n2 || n1 === n3 || n2 === n3) {
+    // Output: if are two numbers equal
+    console.log('\nHá 2 números iguais!');
+  } else {
+    // Output: if all numbers are different
+    console.log('\nTodos os números são diferentes!');
+  }
 }
 
 main();
-
-// Check how many equal numbers exist between three numbers
-function areEqual(a, b, c) {
-  if (a === b && a === c) {
-    return 'Os três números são iguais!';
-  } else if (a === b || a === c || b === c) {
-    return 'Há 2 números iguais!';
-  } else {
-    return 'Todos os números são diferentes!';
-  }
-}
