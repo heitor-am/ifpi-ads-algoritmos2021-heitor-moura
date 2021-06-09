608e74a14edce9717048b521a5b6a013e0adf183
Heitor Moura <heitorandrade2001@gmail.com>
2021-06-09 01:20:34 -0300

    question 1 solved

---
+++ prova_pratica/01_covid-19.js
@@ -0,0 +1,32 @@
+// Include the prompt-sync module
+const input = require('prompt-sync')();
+
+function main() {
+  console.log('### Casos de COVID-19 ###\n');
+
+  // Read the user's input
+  let averageToday = Number(input('Informe a média de casos de COVID-19 hoje: ')),
+    average14DaysAgo = Number(input('Informe a média de casos de 14 dias atrás: '));
+
+  // Process
+  let variation = calculateVariation(averageToday, average14DaysAgo),
+    situation;
+
+  if (Math.abs(variation) < 15) {
+    situation = 'Em estabilidade!';
+  } else if (variation >= 15) {
+    situation = 'Em alta!';
+  } else {
+    situation = 'Em queda!';
+  }
+
+  // Output
+  console.log(`\n${situation} Variação de ${variation}%`);
+}
+
+main();
+
+// Calculate the variation between the averages
+function calculateVariation(today, before) {
+  return ((today - before) / before * 100).toFixed(2);
+}
