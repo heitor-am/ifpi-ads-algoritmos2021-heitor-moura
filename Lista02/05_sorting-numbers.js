3d5c634e36dc751910be562e321d0d09c9931605
Heitor Moura <heitorandrade2001@gmail.com>
2021-06-10 16:31:43 -0300

    ✅ question 5 review

--- Lista02/05_sorting-numbers.js
+++ Lista02/05_sorting-numbers.js
@@ -5,40 +5,23 @@ function main() {
   console.log('### Números em ordem crescente ###\n');
 
   // Read the user's input
-  let n1 = Number(input('Informe o 1° número: ')),
-    n2 = Number(input('Informe o 2° número: ')),
-    n3 = Number(input('Informe o 3° número: '));
+  let a = Number(input('Informe o 1° número: ')),
+    b = Number(input('Informe o 2° número: ')),
+    c = Number(input('Informe o 3° número: '));
 
-  // Get the result
-  let result = sortNumbers(n1, n2, n3);
+  // Sort the numbers
+  let bigger, smaller, middle;
+
+  if (a > b && a > c) {
+    [bigger, middle, smaller] = (b < c) ? [a, c, b] : [a, b, c]
+  } else if (a < b && a < c) {
+    [bigger, middle, smaller] = (b > c) ? [b, c, a] : [c, b, a]
+  } else {
+    [bigger, middle, smaller] = (b > c) ? [b, a, c] : [c, a, b]
+  }
 
   // Output
-  console.log(`\nResultado: ${result}`);
+  console.log(`\nResultado: (${smaller}, ${middle}, ${bigger})`);
 }
 
 main();
-
-// Sort three numbers (only for three numbers) 
-function sortNumbers(a, b, c) {
-  if (a < b) {
-    if (b < c) {
-      return `(${a}, ${b}, ${c})`;
-    } else {
-      if (a < c) {
-        return `(${a}, ${c}, ${b})`;
-      } else {
-        return `(${c}, ${a}, ${b})`;
-      }
-    }
-  } else {
-    if (b < c) {
-      if (a < c) {
-        return `(${b}, ${a}, ${c})`;
-      } else {
-        return `(${b}, ${c}, ${a})`;
-      }
-    } else {
-      return `(${c}, ${b}, ${a})`;
-    }
-  }
-}
